a57c14c02bf5cbc2c2eed48bcb63499ee8f6e8b1
acandido-tech <candidonedved@hotmail.com>
2020-08-04 10:59:03 +0200

    adding scss dependency + init parallax style

--- django/webapp/static/webapp/js/scripts.js
+++ django/webapp/static/webapp/js/scripts.js
@@ -1,13 +1,8 @@
-/*!
-    * Start Bootstrap - Freelancer v6.0.4 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-    (function($) {
-    "use strict"; // Start of use strict
-  
-    // init parallaxSlider
-    $('#pxs_container').parallaxSlider();
-  
-  })(jQuery); // End of use strict
+(function($) {
+  "use strict"; // Start of use strict
+
+  // init parallaxSlider
+  $('#pxs_container').parallaxSlider();
+
+})(jQuery); // End of use strict
   
--- django/webapp/views.py
+++ django/webapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    template = loader.get_template('webapp/index.html')
+    template = loader.get_template('webapp/home.html')
     return HttpResponse(template.render(request=request))
 
 def portfolio(request):
