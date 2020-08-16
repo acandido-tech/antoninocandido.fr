ccfb3b10e03afd1b2e53ee5554c0e1adfef0d663
acandido-tech <candidonedved@hotmail.com>
2020-08-16 21:00:22 +0200

    implement dyanmic build of project content link to bdd

--- django/webapp/views/portfolio.py
+++ django/webapp/views/portfolio.py
@@ -1,5 +1,5 @@
 from .base import BaseView
-from webapp.models import ProjectType
+from webapp.models import Project, ProjectType
 
 
 class PortfolioView(BaseView):
@@ -15,189 +15,8 @@ class PortfolioView(BaseView):
         """ Build data to manage portfolio view"""
         return {
             "project_menu": self.build_portfolio_menu(),
-            "project_list": [
-                {
-                    "name": "edition-limitee",
-                    "title": "EDITION-LIMITEE",
-                    "sub_title": "Site E-commerce",
-                    "type": "sites",
-                    "img_hash": {
-                        "path": "clients/edition-limitee.jpg",
-                        "alt": "vignette edition limitee",
-                    },
-                },
-                {
-                    "name": "ferme-des-sonnailles",
-                    "title": "Ferme des Sonnailles",
-                    "sub_title": "Site Vitrine",
-                    "type": "sites",
-                    "img_hash": {
-                        "path": "clients/sonnailles.jpg",
-                        "alt": "vignette ferme des sonnailles",
-                    },
-                },
-                {
-                    "name": "domaine-saint-andree",
-                    "title": "Domaine des Sonnailles",
-                    "sub_title": "Site Vitrine",
-                    "type": "sites",
-                    "img_hash": {
-                        "path": "clients/domaine-saint-andre.jpg",
-                        "alt": "vignette domaine saint andre",
-                    },
-                },
-                {
-                    "name": "internet-cmwa",
-                    "title": "Internet-cmwa",
-                    "sub_title": "Site Vitrine",
-                    "type": "sites",
-                    "img_hash": {
-                        "path": "clients/internet-cmwa.jpg",
-                        "alt": "vignette internet cmwa",
-                    },
-                },
-                {
-                    "name": "menuiserie-candido",
-                    "title": "Menuiserie Candido",
-                    "sub_title": "Site Vitrine",
-                    "type": "sites",
-                    "img_hash": {
-                        "path": "clients/menuiserie-candido.jpg",
-                        "alt": "vignette menuiserie candido",
-                    },
-                },
-                {
-                    "name": "intranet-ft",
-                    "title": "France Télécom",
-                    "sub_title": "Intranet",
-                    "type": "sites",
-                    "img_hash": {
-                        "path": "clients/orange.jpg",
-                        "alt": "vignette france télécom",
-                    },
-                },
-                {
-                    "name": "credit-agricole",
-                    "title": "Crédit Agricole",
-                    "sub_title": "Webdesign",
-                    "type": "webdesigns",
-                    "img_hash": {
-                        "path": "clients/credit-agricole.jpg",
-                        "alt": "vignette crédit agricole",
-                    },
-                },
-            ],
+            "project_list": self.build_portfolio_content(),
         }
-
-    """
-
-                <li class="item" rel="webdesigns">
-                    <div class="info_cache 7" id="bnp-paribas-web">
-                        <p>
-                            <span>BNP PARIBAS CARDIF</span>
-                            <br />Divers Webdesign
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/bnp-paribas-web.jpg' %}"
-                        alt="vignette BNP Paribas web" />
-                </li>
-
-                <li class="item" rel="webdesigns">
-                    <div class="info_cache 8" id="intranet-gap">
-                        <p>
-                            <span>Mairie de Gap</span>
-                            <br />Intranet
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/intranet-gap.jpg' %}"
-                        alt="vignette mairie de Gap" />
-                </li>
-
-                <li class="item" rel="webdesigns">
-                    <div class="info_cache 9" id="cnad">
-                        <p>
-                            <span>Chez-soi Services</span>
-                            <br />Webdesign
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/cnad.jpg' %}" alt="vignette CNAD" />
-                </li>
-
-                <li class="item" rel="webdesigns">
-                    <div class="info_cache 10" id="apero">
-                        <p>
-                            <span>L'Apero.net</span>
-                            <br />Webdesign
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/lapero-net.jpg' %}" alt="vignette lapero.net" />
-                </li>
-
-                <li class="item" rel="flash">
-                    <div class="info_cache 11" id="etsi">
-                        <p>
-                            <span>ETSI</span>
-                            <br />Flash
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/etsi.jpg' %}" alt="vignette ETSI" />
-                </li>
-
-                <li class="item" rel="flash">
-                    <div class="info_cache 12" id="bnp-senegal">
-                        <p>
-                            <span>BNP Paribas - Sénégal</span>
-                            <br />Flash
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/bnp-senegal.jpg' %}"
-                        alt="vignette BNP Paribas - senegal" />
-                </li>
-
-                <li class="item" rel="flash">
-                    <div class="info_cache 13" id="bnp-carte-france">
-                        <p class="autre_2">
-                            <span>BNP Paribas<br />Carte de France</span>
-                            <br />Flash
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/bnp-carte-france.jpg' %}"
-                        alt="vignette BNP Paribas - carte de France" />
-                </li>
-
-                <li class="item" rel="flash">
-                    <div class="info_cache 14" id="bnp-banniere">
-                        <p>
-                            <span>BNP Paribas</span>
-                            <br />Bannières Flash
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/bnp-mix.jpg' %}"
-                        alt="vignette BNP Paribas - bannières" />
-                </li>
-
-                <li class="item" rel="videos-3d">
-                    <div class="info_cache 15" id="bnp-paribas-video">
-                        <p>
-                            <span>BNP Paribas</span>
-                            <br />Vidéos
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/bnp-paribas-video.jpg' %}"
-                        alt="vignette BNP Paribas - vidéos" />
-                </li>
-
-                <li class="item" rel="videos-3d">
-                    <div class="info_cache 16" id="domaine-3d">
-                        <p>
-                            <span>Domaine Saint-André/span>
-                                <br />Modélisation 3D
-                        </p>
-                    </div>
-                    <img src="{% static 'webapp/assets/img/clients/domaine-saint-andre-3d.jpg' %}"
-                        alt="vignette 3D domaine saint andre" />
-                </li>
-    """
 
     def build_portfolio_menu(self):
         """ Build data to manage portfolio menu"""
@@ -208,3 +27,20 @@ class PortfolioView(BaseView):
             )
         return menu_list
 
+    def build_portfolio_content(self):
+        """ Build data to manage portfolio content"""
+        project_list = []
+        for project_hash in Project.objects.all().filter(active=1):
+            project_list.append(
+                {
+                    "name": project_hash.name,
+                    "title": project_hash.title,
+                    "sub_title": project_hash.sub_title,
+                    "type": project_hash.project_type.name,
+                    "img_hash": {
+                        "path": project_hash.project_image.vignette_path,
+                        "alt": project_hash.project_image.vignette_alt,
+                    },
+                }
+            )
+        return project_list
