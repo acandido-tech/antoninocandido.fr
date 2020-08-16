c7acf5ca478c5a4c31a3dcfb8896b9f36a5451d4
acandido-tech <candidonedved@hotmail.com>
2020-08-16 11:04:14 +0200

    add project img data

--- django/webapp/models.py
+++ django/webapp/models.py
@@ -13,8 +13,9 @@ class ProjectType(models.Model):
 
 class ProjectImage(models.Model):
     path = models.CharField(max_length=45)
-    alt = models.CharField(max_length=45)
+    alt = models.CharField(max_length=45, default="")
     vignette_path = models.CharField(max_length=45)
+    vignette_alt = models.CharField(max_length=45, default="")
 
 
 class Project(models.Model):
